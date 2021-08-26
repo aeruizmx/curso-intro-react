5a286b5375495eb29df3ca0aa4c71d64ea7604ce
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-08-26 10:03:02 -0500

    Clase 8

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch(){
+  
+  const [searchValue, setSearchValue] = React.useState('')
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value)
+    setSearchValue(event.target.value);
   }
 
-  return (
-    <input className="TodoSearch" placeholder="Cebolla" onChange={onSearchValueChange} />
-  );
+  return [
+    <input className="TodoSearch" placeholder="Cebolla" onChange={onSearchValueChange} value={searchValue} />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
